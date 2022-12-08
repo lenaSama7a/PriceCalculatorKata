d3c0b8e991cf03ecaf14fc55c1638a6d72bfb6d2
lenaSamaha <lena.sama7a@gmail.com>
2022-12-08 20:50:20 +0200

    Adding the first requirment - TAX

---
+++ PriceCalculatorKata/UserInterface.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PriceCalculatorKata
+{
+    public class UserInterface
+    {
+        public static void SetTaxByCustomer()
+        {
+            Print.AskCustomerAboutTaxAmount();
+            Tax.TaxAmount = Convert.ToDecimal(Console.ReadLine());
+        }
+    }
+}
